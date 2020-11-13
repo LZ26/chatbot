4df8dae8ef14b635fb3940fb2c39dfae8aa5dcc6
Laziz <laziz.zokir26@gmail.com>
2020-11-13 01:04:35 -0500

    added some styling to chat screen

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { userMessage, sendMessage } from '../store/redux/chats';
 
 const Chat = ({ chat, userMessage, sendMessage }) => {
   //handle user message
   const [message, setMessage] = useState('');
+  const messageEnd = useRef(null);
 
   //function that handles user submission
+  const scrollToBottom = () => {
+    messageEnd.current.scrollIntoView({ behavior: 'smooth' });
+  };
+  useEffect(scrollToBottom, [chat]);
+
   const handleClick = async (ev) => {
     const code = ev.keyCode || ev.which;
 
@@ -20,16 +26,22 @@ const Chat = ({ chat, userMessage, sendMessage }) => {
 
   return (
     <div className="chat">
-      <h1>Customer Care Chatbot (CCBot)</h1>
-      {chat.length === 0
-        ? ''
-        : chat.map((txt) => {
-            return (
-              <div key={txt.id} className={txt.type}>
-                {txt.message}
-              </div>
-            );
-          })}
+      {/* <div>
+        <h1>Customer Care Chatbot (CCBot)</h1>
+      </div> */}
+      <div className="chat-history">
+        {chat.length === 0
+          ? ''
+          : chat.map((txt) => {
+              return (
+                <div key={txt.id} className={txt.type}>
+                  {txt.message}
+                </div>
+              );
+            })}
+        <div className="scroller" ref={messageEnd}></div>
+      </div>
+
       <input
         id="chatField"
         onChange={(ev) => setMessage(ev.target.value)}

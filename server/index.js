1453fb891bf6da143026828f2a29bfd4ab00df4c
Laziz <laziz.zokir26@gmail.com>
2020-11-14 02:09:40 -0500

    final touchups v2

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-const volleyball = require('volleyball');
 const app = express();
 require('dotenv').config();
 
 // logging middleware
 // Only use logging middleware when not running tests
-const debug = process.env.NODE_ENV === 'test';
-app.use(volleyball.custom({ debug }));
 
 // body parsing middleware
 app.use(express.json());
